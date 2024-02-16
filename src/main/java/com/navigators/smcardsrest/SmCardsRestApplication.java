835cc33c66a1cd17c6f9c71153c4ea57d9e998db
Sean <54630058+seanang99@users.noreply.github.com>
2024-02-16 18:02:15 +0800

    Add Spring Boot template files

---
+++ src/main/java/com/navigators/smcardsrest/SmCardsRestApplication.java
@@ -0,0 +1,13 @@
+package com.navigators.smcardsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmCardsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmCardsRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navigators/smcardsrest/SmCardsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navigators.smcardsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmCardsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
